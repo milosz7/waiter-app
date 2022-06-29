3e917279b186b31e9523138db526f309192ddf7d
Milosz B <miloszbargiel@gmail.com>
2022-06-29 19:04:39 +0200

    Add BrowserRouter component and route for homepage

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
