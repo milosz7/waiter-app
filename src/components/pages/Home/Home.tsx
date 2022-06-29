c059f088dc93f22a314312d75670df989d87176f
Milosz B <miloszbargiel@gmail.com>
2022-06-29 20:53:07 +0200

    Display tablesList on homepage

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
+import TablesList from '../../features/TablesList/TablesList';
+
 const Home = () => {
   return (
-    <h1>All tables</h1>
-  )
-}
+    <>
+      <h1 className="py-3">All tables</h1>
+      <TablesList />
+    </>
+  );
+};
 
-export default Home
+export default Home;
