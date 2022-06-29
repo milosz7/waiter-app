7035db44509df90b8e3397470738fbd97b2ec13b
Milosz B <miloszbargiel@gmail.com>
2022-06-29 20:48:11 +0200

    Add data fetching on page load

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import Home from './components/pages/Home/Home';
 import { Container } from 'react-bootstrap';
 import Navigation from './components/views/Navigation/Navigation';
 import { Routes, Route } from 'react-router';
+import { useAppDispatch } from './redux/hooks';
+import { fetchTablesData } from './redux/slices/tablesSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => void dispatch(fetchTablesData()), [dispatch]);
+
   return (
     <Container>
       <Navigation />
