526abf3f776a717971d237a4691010493c9af5ea
Milosz B <miloszbargiel@gmail.com>
2022-07-01 15:27:12 +0200

    Add navigate to homepage when passed wrong table url, add spinner and modify input props of tableForm

--- src/components/features/TableForm/TableForm.tsx
+++ src/components/features/TableForm/TableForm.tsx
@@ -1,17 +1,18 @@
 import { Form, Button } from 'react-bootstrap';
-import { getTableById } from '../../../redux/slices/tablesSlice';
-import { useAppSelector } from '../../../redux/hooks';
 import { useState } from 'react';
 import styles from './TableForm.module.scss';
 
-const TableForm = ({ id }: { id: string | undefined }) => {
-  const tableData = useAppSelector((state) => getTableById(state, id));
-  const tableStatuses = ['Busy', 'Free', 'Cleaning', 'Reserved'];
-  const [formData, setFormData] = useState(tableData);
+interface Data {
+  id: number;
+  status: string;
+  currentPeopleNumber: number;
+  maxPeopleNumber: number;
+  bill: number;
+}
 
-  if (!tableData || !formData) return null;
-  
-  console.log(formData)
+const TableForm = ({ data }: { data: Data }) => {
+  const tableStatuses = ['Busy', 'Free', 'Cleaning', 'Reserved'];
+  const [formData, setFormData] = useState(data);
 
   const changeMin = (value: string) => {
     const newValue = parseInt(value) ? parseInt(value) : 0;
