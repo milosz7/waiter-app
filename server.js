f7bef6d7dbc453ec95966f62708f0b625312f4fc
Milosz B <miloszbargiel@gmail.com>
2022-07-03 20:20:16 +0200

    Change the route of db json file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./build/db/app.json');
+const router = jsonServer.router('./build/db/db.json');
 const middlewares = jsonServer.defaults({
   static: './build',
   noCors: true
